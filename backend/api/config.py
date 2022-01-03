d9bb64907f366122b84d987888bc0b6674d6bda9
ryutaro-0907 <ryutaro.furutani@gmail.com>
2022-01-03 16:45:03 -0600

    update

--- backend/api/config.py
+++ backend/api/config.py
@@ -5,8 +5,8 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     """Settings."""
 
-    file_storage: str  # S3, Local
+    file_storage: str
     aws_access_key_id: str
     aws_secret_access_key: str
-    aws_default_region = 'ap-northeast-1'
-    bucket_name = 'hacarus-underground-staging-data'
+    aws_default_region = ''
+    bucket_name = ''
